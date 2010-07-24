7b13cc4a08be92122cb528fa0749bb68d2678d57
habibnahas <habibnahas@hdesk.(none)>
2010-07-24 13:07:19 +0530

    ri and its dependencies

--- hn-ruby.el
+++ hn-ruby.el
@@ -7,3 +7,6 @@
 (autoload 'run-ruby "inf-ruby" "Run an inferior Ruby process")
 (autoload 'inf-ruby-keys "inf-ruby" "Set local key defs for inf-ruby in ruby-mode")
 (add-hook 'ruby-mode-hook '(lambda () (inf-ruby-keys)))
+
+;;ri
+(require 'ri)
