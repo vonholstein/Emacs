eed8af3e00e55688589cb3ce0d33b0f892609087
habibnahas <habibnahas@hdesk.(none)>
2010-07-24 13:06:35 +0530

    hn-startup, disabled ansi mode

---
+++ hn-startup.el
@@ -0,0 +1,10 @@
+(menu-bar-mode 1)
+(tool-bar-mode 0)
+(global-font-lock-mode t)
+(setq visible-bell t) ;;disable system beep
+(setq inhibit-startup-message t) ;;disable emacs start screen
+
+;; escape funny characters in emacs shell
+;; from http://stackoverflow.com/questions/704616/something-wrong-with-emacs-shell
+;;(autoload 'ansi-color-for-comint-mode-on "ansi-color" nil t)
+;;(add-hook 'term-mode-hook 'ansi-color-for-comint-mode-on)
