1c916ac8646bdeb13813b6eebc0d9d2233da98ab
habibnahas <habibnahas@gmail.com>
2010-07-31 23:47:36 +0530

    added org-mode settings to .emacs and hn-org.el, mostly from http://doc.norang.ca/org-mode.html

--- .emacs
+++ .emacs
@@ -50,6 +50,10 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (load-library "hn-misc")
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; org-mode
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(load-library "hn-org")
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Ruby
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (load-file (concat emacs-lib-root "//hn-ruby.el"))
@@ -69,11 +73,12 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "black" :foreground "#c0c0c0" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "unknown" :family "DejaVu Sans Mono"))))
- '(org-todo ((t (:foreground "saddle brown" :weight bold)))))
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "#c0c0c0" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
+ 
 (custom-set-variables
   ;; custom-set-variables was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(ecb-options-version "2.40"))
+
--- hn-org.el
+++ hn-org.el
@@ -3,8 +3,221 @@
 
 (define-key global-map [(control meta ?r)] 'remember)
 
-(custom-set-variables
- '(org-agenda-files (quote ("~/org/todo.org")))
- '(org-default-notes-file "~/notes.org"))
- 
- 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;; Key Bindings ;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Custom Key Bindings
+(global-set-key (kbd "<f12>") 'org-agenda)
+(global-set-key (kbd "<f5>") 'bh/org-todo)
+(global-set-key (kbd "<S-f5>") 'bh/widen)
+(global-set-key (kbd "<f7>") 'set-truncate-lines)
+(global-set-key (kbd "<f8>") 'org-cycle-agenda-files)
+(global-set-key (kbd "<f9> b") 'bbdb)
+(global-set-key (kbd "<f9> c") 'calendar)
+(global-set-key (kbd "<f9> f") 'boxquote-insert-file)
+(global-set-key (kbd "<f9> g") 'gnus)
+(global-set-key (kbd "<f9> h") 'bh/hide-other)
+
+(defun bh/hide-other ()
+  (interactive)
+  (save-excursion
+    (org-back-to-heading)
+    (org-shifttab)
+    (org-reveal)
+    (org-cycle)))
+
+(global-set-key (kbd "<f9> i") 'bh/org-info)
+
+(defun bh/org-info ()
+  (interactive)
+  (info "~/git/org-mode/doc/org.info"))
+
+(global-set-key (kbd "<f9> I") 'bh/clock-in)
+(global-set-key (kbd "<f9> O") 'bh/clock-out)
+(global-set-key (kbd "<f9> r") 'boxquote-region)
+(global-set-key (kbd "<f9> s") 'bh/go-to-scratch)
+
+(defun bh/go-to-scratch ()
+  (interactive)
+  (switch-to-buffer "*scratch*")
+  (delete-other-windows))
+
+(global-set-key (kbd "<f9> t") 'bh/insert-inactive-timestamp)
+(global-set-key (kbd "<f9> u") 'bh/untabify)
+
+(defun bh/untabify ()
+  (interactive)
+  (untabify (point-min) (point-max)))
+
+(global-set-key (kbd "<f9> v") 'visible-mode)
+(global-set-key (kbd "<f9> SPC") 'bh/clock-in-interrupted-task)
+(global-set-key (kbd "C-<f9>") 'previous-buffer)
+(global-set-key (kbd "C-x n r") 'narrow-to-region)
+(global-set-key (kbd "C-<f10>") 'next-buffer)
+(global-set-key (kbd "<f11>") 'org-clock-goto)
+(global-set-key (kbd "C-<f11>") 'org-clock-in)
+(global-set-key (kbd "C-s-<f12>") 'bh/save-then-publish)
+(global-set-key (kbd "M-<f11>") 'org-resolve-clocks)
+(global-set-key (kbd "C-M-r") 'org-remember)
+(global-set-key (kbd "M-<f9>") (lambda ()
+                                 (interactive)
+                                 (unless (buffer-modified-p)
+                                   (kill-buffer (current-buffer)))
+                                 (delete-frame)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;; Agenda setup ;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(setq org-agenda-files (quote ("~/org/refile.org"
+			       "~/org/todo.org"
+			       "~/org/notes.org")))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;; Todo setup ;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(setq org-todo-keywords (quote ((sequence "TODO(t@/!)" "NEXT(n)" "|" "DONE(d!/!)")
+ (sequence "WAITING(w@/!)" "SOMEDAY(s!)" "|" "CANCELLED(c@/!)"))))
+
+(setq org-todo-keyword-faces
+      (quote (("TODO"      :foreground "red"          :weight bold)
+              ("NEXT"      :foreground "blue"         :weight bold)
+              ("DONE"      :foreground "forest green" :weight bold)
+              ("WAITING"   :foreground "yellow"       :weight bold)
+              ("SOMEDAY"   :foreground "goldenrod"    :weight bold)
+              ("CANCELLED" :foreground "orangered"    :weight bold))))
+
+(setq org-use-fast-todo-selection t)
+
+;; Change task state to NEXT from TODO when clocking in
+(defun bh/clock-in-to-next (kw)
+  "Switch task from TODO to NEXT when clocking in.
+Skips remember tasks and tasks with subtasks"
+  (if (and (string-equal kw "TODO")
+           (not (string-equal (buffer-name) "*Remember*")))
+      (let ((subtree-end (save-excursion (org-end-of-subtree t)))
+            (has-subtask nil))
+        (save-excursion
+          (forward-line 1)
+          (while (and (not has-subtask)
+                      (< (point) subtree-end)
+                      (re-search-forward "^\*+ " subtree-end t))
+            (when (member (org-get-todo-state) org-not-done-keywords)
+              (setq has-subtask t))))
+        (when (not has-subtask)
+          "NEXT"))))
+
+
+(setq org-clock-in-switch-to-state (quote bh/clock-in-to-next))
+
+(setq org-default-notes-file "~/git/org/refile.org")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;  Load Org Remember Stuff ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(setq org-default-notes-file "~/org/refile.org")
+
+(org-remember-insinuate)
+
+;; Start clock in a remember buffer and switch back to previous clocking task on save
+(add-hook 'remember-mode-hook 'org-clock-in 'append)
+(add-hook 'org-remember-before-finalize-hook 'bh/clock-in-interrupted-task)
+
+;; I use C-M-r to start org-remember
+(global-set-key (kbd "C-M-r") 'org-remember)
+
+;; Keep clocks running
+(setq org-remember-clock-out-on-exit nil)
+
+;; C-c C-c stores the note immediately
+(setq org-remember-store-without-prompt t)
+
+;; I don't use this -- but set it in case I forget to specify a location in a future template
+(setq org-remember-default-headline "Tasks")
+
+;; 3 remember templates for TODO tasks, Notes, and Phone calls
+(setq org-remember-templates (quote (("todo" ?t "* TODO %?\n  %U\n  %a" nil bottom nil)
+                                     ("note" ?n "* %?                                                                            :NOTE:\n  %U\n  %a\n  :CLOCK:\n  :END:" nil bottom nil)
+                                     ("appointment" ?a "* %?\n  %U" "~/git/org/todo.org" "Appointments" nil)
+                                     ("org-protocol" ?w "* TODO Review %c%!\n  %U" nil bottom nil))))
+
+
+;;;;;;;;;;;;;;;;;;;
+;;; Clock Setup ;;;
+;;;;;;;;;;;;;;;;;;;
+
+;;
+;; Resume clocking tasks when emacs is restarted
+(org-clock-persistence-insinuate)
+;;
+;; Yes it's long... but more is better ;)
+(setq org-clock-history-length 28)
+;; Resume clocking task on clock-in if the clock is open
+(setq org-clock-in-resume t)
+;; Change task state to NEXT when clocking in
+(setq org-clock-in-switch-to-state (quote bh/clock-in-to-next))
+;; Separate drawers for clocking and logs
+(setq org-drawers (quote ("PROPERTIES" "LOGBOOK" "CLOCK")))
+;; Save clock data in the CLOCK drawer and state changes and notes in the LOGBOOK drawer
+(setq org-clock-into-drawer "CLOCK")
+;; Sometimes I change tasks I'm clocking quickly - this removes clocked tasks with 0:00 duration
+(setq org-clock-out-remove-zero-time-clocks t)
+;; Clock out when moving task to a done state
+(setq org-clock-out-when-done t)
+;; Save the running clock and all clock history when exiting Emacs, load it on startup
+(setq org-clock-persist (quote history))
+;; Enable auto clock resolution for finding open clocks
+(setq org-clock-auto-clock-resolution (quote when-no-clock-is-running))
+;; Include current clocking task in clock reports
+(setq org-clock-report-include-clocking-task t)
+
+(setq bh/keep-clock-running nil)
+
+(defun bh/clock-in ()
+  (interactive)
+  (setq bh/keep-clock-running t)
+  (if (marker-buffer org-clock-default-task)
+      (unless (org-clock-is-active)
+        (bh/clock-in-default-task))
+    (unless (marker-buffer org-clock-default-task)
+      (org-agenda nil "c"))))
+
+(defun bh/clock-out ()
+  (interactive)
+  (setq bh/keep-clock-running nil)
+  (when (org-clock-is-active)
+    (org-clock-out)))
+
+(defun bh/clock-in-default-task ()
+  (save-excursion
+    (org-with-point-at org-clock-default-task
+      (org-clock-in))))
+
+(defun bh/clock-out-maybe ()
+  (when (and bh/keep-clock-running (not org-clock-clocking-in) (marker-buffer org-clock-default-task))
+    (bh/clock-in-default-task)))
+
+(add-hook 'org-clock-out-hook 'bh/clock-out-maybe 'append)
+
+(defun bh/clock-in-interrupted-task ()
+  "Clock in the interrupted task if there is one"
+  (interactive)
+  (let ((clock-in-to-task))
+    (if (org-clock-is-active)
+        (when (marker-buffer org-clock-interrupted-task)
+          (if (equal org-clock-interrupted-task org-clock-hd-marker)
+              (setq clock-in-to-task (cadr org-clock-history))
+            (setq clock-in-to-task org-clock-interrupted-task))))
+    (if clock-in-to-task
+        (org-with-point-at clock-in-to-task
+          (org-clock-in nil))
+      (org-clock-out))))
+
+(defun bh/insert-inactive-timestamp ()
+  (interactive)
+  (org-insert-time-stamp nil t t nil nil nil))
+
+			     
