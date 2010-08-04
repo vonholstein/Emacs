d2b6ba70394a58a46351f6a3c1c80088698f6b6d
habibnahas <habibnahas@gmail.com>
2010-08-04 22:39:05 +0530

    changed color theme to billw

--- hn-color-theme.el
+++ hn-color-theme.el
@@ -1,6 +1,13 @@
 ;; Set blackboard as color theme
+;; (eval-after-load "color-theme"
+;;   '(progn
+;;      (color-theme-initialize)
+;;      (load-file "~/lib-emacs/vendor/color-theme/color-theme-blackboard.el")     
+;;      (color-theme-hober)))
+
 (eval-after-load "color-theme"
   '(progn
      (color-theme-initialize)
-     (load-file "~/lib-emacs/vendor/color-theme/color-theme-blackboard.el")     
-     (color-theme-hober)))
+     (color-theme-billw)))
+
+
