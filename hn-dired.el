e152da2445de9d9e011ff27bc54c6cc4bb370d6b
Habib Nahas <habibnahas@gmail.com>
2011-01-04 10:56:55 +0530

    disable open new dired buffer on return or ^

--- hn-dired.el
+++ hn-dired.el
@@ -1,1 +1,10 @@
 (put 'dired-find-alternate-file 'disabled nil)
+
+(add-hook 'dired-mode-hook
+	  (lambda ()
+	    (define-key dired-mode-map (kbd "<return>")
+	      'dired-find-alternate-file) ; was dired-advertised-find-file
+	    (define-key dired-mode-map (kbd "^")
+	      (lambda () (interactive) (find-alternate-file "..")))
+					; was dired-up-directory
+	    ))
