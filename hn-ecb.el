b46a821012cb1e948b3392779d6d9766fdefee58
habibnahas <habibnahas@habibnahas-desktop.(none)>
2011-01-17 21:11:28 +0530

    Added rails_projects to ecb favs

--- hn-ecb.el
+++ hn-ecb.el
@@ -1,5 +1,5 @@
 (setq ecb-windows-width 0.15)
 (setq ecb-tip-of-the-day nil) ;;disable ecb tip of the day
 (setq ecb-options-version "2.40")
-(setq ecb-source-path (quote (("~/ruby-projects/sinatra" "sinatra") ("~/org" "org") ("~/lib-emacs" "lib-emacs"))))
+(setq ecb-source-path (quote (("~/org" "org") ("~/lib-emacs" "lib-emacs") ("~/rails_projects" "rails-projects"))))
 (global-set-key (kbd "<f6>") 'ecb-toggle-ecb-windows)
