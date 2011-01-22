34536c026a8e0777c7b84a1412ad2aba8f340aee
habibnahas <habibnahas@habibnahas-desktop.(none)>
2011-01-22 11:37:41 +0530

    commented out find-program, changed shell mode hookf for ansi-color

--- hn-startup.el
+++ hn-startup.el
@@ -6,14 +6,13 @@
 
 ;; escape funny characters in emacs shell
 ;; from http://stackoverflow.com/questions/704616/something-wrong-with-emacs-shell
-(autoload 'ansi-color-for-comint-mode-on "ansi-color" nil t)
+;(autoload 'ansi-color-for-comint-mode-on "ansi-color" nil t)
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
-
 ;; Redefine C-x C-b to ibuffer
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 
 ;; Use gnu find for find-file
-(setq find-program "c:\\gnuwin32\\bin\\find.exe")
+;;(setq find-program "c:\\gnuwin32\\bin\\find.exe")
 
 ;; enable epa
 (epa-file-enable)
