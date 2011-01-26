f35368651f1baf877d29df27b816bfe21153c11b
habibnahas <habibnahas@habibnahas-desktop.(none)>
2011-01-26 11:40:24 +0530

    Set colors for multi-term

--- hn-multi-term.el
+++ hn-multi-term.el
@@ -1,1 +1,4 @@
 (setq multi-term-program "/bin/bash")
+(custom-set-variables
+     '(term-default-bg-color "#000000")        ;; background color (black)
+     '(term-default-fg-color "#dddd00"))       ;; foreground color (yellow)
