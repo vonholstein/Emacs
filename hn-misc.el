cf1e1b2ec02ab0f4bdee8b1df10c9a0254240cec
Habib Nahas <hnahas@hnahas.desktop.amazon.com>
2011-09-28 12:10:36 +0530

    enabled hippie-expand

--- hn-misc.el
+++ hn-misc.el
@@ -45,3 +45,6 @@
 ;(setq uniquify-separator "/")
 (setq uniquify-after-kill-buffer-p t) ; rename after killing uniquified
 (setq uniquify-ignore-buffers-re "^\\*") ; don't muck with special buffers
+
+;; enable hippie-expand
+(global-set-key (kbd "M-/") 'hippie-expand)
