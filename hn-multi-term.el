73e9ad4ffe70e46f33f7da8d280c0f518e74e43a
Habib Nahas <hnahas@hnahas.desktop.amazon.com>
2011-09-28 11:46:43 +0530

    set multi-term fg,bg colors

--- hn-multi-term.el
+++ hn-multi-term.el
@@ -1,4 +1,4 @@
-;;(custom-set-variables
-;;     '(term-default-bg-color "#000000")        ;; background color (black)
-;;     '(term-default-fg-color "#dddd00"))       ;; foreground color (yellow)
+(custom-set-variables
+     '(term-default-bg-color "#000000")        ;; background color (black)
+     '(term-default-fg-color "#dddd00"))       ;; foreground color (yellow)
 ;;(setq multi-term-program (expand-file-name "~/zshell"))
