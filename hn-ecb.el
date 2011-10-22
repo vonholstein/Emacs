4e7b54766109176b3061e78eb9c8c91810cc1fc9
habib <habib@habib-VirtualBox.(none)>
2011-10-22 15:10:36 +0530

    added directories to ecb-source-path

--- hn-ecb.el
+++ hn-ecb.el
@@ -1,5 +1,5 @@
 (setq ecb-windows-width 0.15)
 (setq ecb-tip-of-the-day nil) ;;disable ecb tip of the day
 (setq ecb-options-version "2.40")
-(setq ecb-source-path (quote (("~/org" "org") ("~/lib-emacs" "lib-emacs") ("~/rails_projects" "rails-projects"))))
+(setq ecb-source-path (quote (("~/org" "org") ("~/lib-emacs" "lib-emacs") ("~/ruby-code" "ruby-code") ("~/c-code" "c-code"))))
 (global-set-key (kbd "<f6>") 'ecb-toggle-ecb-windows)
