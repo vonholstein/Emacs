2552f078450de1bed0d332bca9a54db5c69f5f07
Habib Nahas <habibnahas@gmail.com>
2018-06-11 12:19:45 -0700

    New root

---
+++ init.el
@@ -0,0 +1,70 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))	     
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; Add in your own as you wish:
+(defvar my-packages '(color-theme helm markdown-mode markdown-preview-mode)
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+(require 'helm-config)
+(helm-mode 1)
+(load-file "~/.emacs.d/color-theme-billw.el")
+
+(setq inhibit-splash-screen t)
+(setq visible-bell t)
+(tool-bar-mode -1)
+
+;; Helm config
+(global-set-key (kbd "M-x") #'helm-M-x)
+(global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
+(global-set-key (kbd "C-x C-f") #'helm-find-files)
+(global-set-key (kbd "C-c l") #'org-store-link)
+
+;; Start maximized
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(initial-frame-alist (quote ((fullscreen . maximized))))
+ '(package-selected-packages
+   (quote
+    (markdown-mode markdown-preview-mode helm color-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
+  backup-by-copying t    ; Don't delink hardlinks
+  version-control t      ; Use version numbers on backups
+  delete-old-versions t  ; Automatically delete excess backups
+  kept-new-versions 20   ; how many of the newest versions to keep
+  kept-old-versions 5    ; and how many of the old
+  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; org-mode configuration
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(setq org-directory "~/org")
+(setq org-agenda-files (quote ("~/org/base.org")))
+
+
+;; Other
+(put 'dired-find-alternate-file 'disabled nil)
+(put 'upcase-region 'disabled nil)
+(setq explicit-shell-file-name "C:/babun/.babun/cygwin/bin/bash.exe")
+(setq shell-file-name explicit-shell-file-name)
+;(setq explicit-bash.exe-args '("--noediting" "--login" "-i"))
+(add-to-list 'exec-path "C:/babun/.babun/cygwin/bin")
+(put 'narrow-to-region 'disabled nil)
